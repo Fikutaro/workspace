3a17ad19f7c95efd0fc982b9bdd66bf8115164c2
Fikutaro <nomio9029@gmail.com>
2021-08-25 21:14:41 +0900

    practice

--- HelloWorld/src/lesson/PointInstanceExample.java
+++ HelloWorld/src/lesson/PointInstanceExample.java
@@ -10,11 +10,15 @@ public class PointInstanceExample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		Point p1 = new Point();
-		p1.x = 10;
-		p1.y = 5;
-		
-		System.out.println("p1のx座標は"+ p1.x);
+		Point[] points = new Point [5];
+		for (int i = 0; i < 5; i++) {
+			points[i] = new Point();
+			points[i].x = i;
+			points[i].y = i * i;
+		}
+		for (int i = 0; i < 5; i++ ) {
+			System.out.println("point["+ i + "]の座標は、("+points[i].x + "," + points[i].y + ")");
+		}
 	}
 
 }
