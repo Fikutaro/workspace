0e672f23e9feab57e0c8286581a7001aaf13c35c
Fikutaro <nomio9029@gmail.com>
2021-08-25 21:06:45 +0900

    practice

---
+++ HelloWorld/src/lesson/PointInstanceExample.java
@@ -0,0 +1,20 @@
+package lesson;
+
+class Point {
+	int x;
+	int y;
+}
+
+public class PointInstanceExample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Point p1 = new Point();
+		p1.x = 10;
+		p1.y = 5;
+		
+		System.out.println("p1のx座標は"+ p1.x);
+	}
+
+}
