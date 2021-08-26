775398e186424c19d368bb2f18f5818fd89cc979
Fikutaro <nomio9029@gmail.com>
2021-08-26 17:16:39 +0900

    practice method

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/49/50e91de74506001c1a58cb4c218f7ad5
@@ -0,0 +1,41 @@
+package lesson;
+
+class Point {
+	int x;
+	int y;
+
+	void printPosition() {
+		System.out.println("座標値は(" + this.x
+				+ "," + this.y + ")です");
+	}
+	void multiply(int n) {
+		this.x *= n;
+		this.y *= n;
+	}
+	
+	void add(int dx, int dy) {
+		this.x += dx;
+		this.y += dy;
+	
+	}
+}
+
+public class MethodCallExample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Point p = new Point();
+		p.x = 10;
+		p.y = 5;
+
+		p.printPosition();
+		
+		p.multiply(2);
+		p.printPosition();
+		
+		p.add(2, 3);
+		p.printPosition();
+
+	}
+}
--- HelloWorld/src/lesson/MethodCallExample.java
+++ HelloWorld/src/lesson/MethodCallExample.java
@@ -4,19 +4,8 @@ class Point {
 	int x;
 	int y;
 
-	void printPosition() {
-		System.out.println("座標値は(" + this.x
-				+ "," + this.y + ")です");
-	}
-	void multiply(int n) {
-		this.x *= n;
-		this.y *= n;
-	}
-	
-	void add(int dx, int dy) {
-		this.x += dx;
-		this.y += dy;
-	
+	int getXY () {
+		return this.x * this.y;
 	}
 }
 
@@ -29,13 +18,7 @@ public class MethodCallExample {
 		p.x = 10;
 		p.y = 5;
 
-		p.printPosition();
-		
-		p.multiply(2);
-		p.printPosition();
-		
-		p.add(2, 3);
-		p.printPosition();
-
+		int xy = p.getXY();
+		System.out.println("pのxとyの値を掛けた結果は" + xy);
 	}
 }
