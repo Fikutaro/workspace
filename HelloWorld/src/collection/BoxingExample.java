61bd828d0e999f082c2eb76f19bffbdfc80479ec
Fikutaro <nomio9029@gmail.com>
2021-08-31 23:46:49 +0900

    practice collection

---
+++ HelloWorld/src/collection/BoxingExample.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class BoxingExample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		ArrayList<Integer> integerList = new ArrayList<Integer>();
+		integerList.add(50);
+		integerList.add(100);
+		int i0 = integerList.get(0);
+		int i1 = integerList.get(1);
+		System.out.println(i0);
+		System.out.println(i1);
+	}
+
+}
