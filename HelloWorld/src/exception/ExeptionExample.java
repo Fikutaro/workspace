51ae7c1e01aa3a0c0b77565614bfb297bac3798b
Fikutaro <nomio9029@gmail.com>
2021-08-31 16:37:10 +0900

    practice method

---
+++ HelloWorld/src/exception/ExeptionExample.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class ExeptionExample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int a = 4;
+		int b = 0;
+		int c = a / b;
+		System.out.println("cの値は" + c);
+		System.out.println("処理を正常に終了します");
+	}
+
+}
